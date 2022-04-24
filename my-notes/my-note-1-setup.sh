076b4a843c6ea68b01934a07b43df618035acc02
wltsankalpa <wltsankalpa@gmail.com>
2022-04-24 23:41:18 +0530

    lap-ubuntu

--- my-notes/my-note-1-setup.sh
+++ my-notes/my-note-1-setup.sh
@@ -1,172 +1,80 @@
 
-```shell
 # Clone repo and inspect docker-compose file
-
 
 git clone https://github.com/VLTSankalpa/data-engineering-spark.git
 cd data-engineering-spark/
 ls -ltrh
 cat docker-compose.yaml
 
-
 # itvdflab service
-
 # Python and jupyter in itvdflab service
-
 # SQL and postgres in pg.itversity.com service
-
 # itvdflab service has volume bind to itversity-material dirctory
-
-
-
 
 docker-compose up -d --build itvdflab 
 
-
-
-
 # This will build itvdflab and pg.itversity.com both docker compose services 
-
 # itvdflab depend on pg.itversity.com
-
-
 # Therefore if we stop pg.itversity.com it will stop itvdflab as well
-
-
-
 
 docker-compose stop pg.itversity.com
 
-
-
-
 # If we try to bring up itvdflab it will bring up pg.itversity.com as well
 
-
-
-
 docker-compose start itvdflab
-
-
 docker-compose ps
 docker image ls
 docker ps
 
-
-
-
 # Get jupyter token from docker python service
-
-
-
 
 sudo docker-compose exec itvdflab bash
 cat .local/share/jupyter/runtime/jpserver-
 .json
-sudo docker-compose exec itvdflab bash -c "cat .local/share/jupyter/runtime/jpserver-
-.json" | grep -i token
-
-
-
+sudo docker-compose exec itvdflab bash -c "cat .local/share/jupyter/runtime/jpserver-*.json" | grep -i token
 
 # To setup and access jupyyerlab hosted in Google cloud VM
 
-
-
-
 sudo apt install pip
-
-
 pip install jupyter lab
-
-
-
 
 # This will start jupyter lab bind to local ip (127.0.0.1)
 
-
-
-
 jupyter lab 
-
-
-
 
 # This will start jupyter lab bind to private ip
 
-
-
-
 jupyter lab --ip 10.182.0.2
 
-
-
-
 # We cant use public ip to start jupyter as the public ip is not bounded to VM (its a load balancer)
-
-
 # We also can run jupyter bind to all the ips with universal ip (0.0.0.0)
-
-
-
 
 jupyter lab --ip 0.0.0.0
 
-
-
-
 # Ones this is done check accesibility via all the ips as follows
-
-
-
 
 telnet localhost 8888
 telnet 10.182.0.2 8888
 telnet <public-ip> 8888
 
-
-
-
 # After that we can use sshuttle as a vpn
-
-
-
 
 sudo apt install sshuttle
 sshuttle -r tharindu_gpc@34.125.220.158 0/0
 
-
-
-
 # Now we can access from our local pc browser to jupyter runtime
-
-
-
 
 http://34.125.220.158:8889/lab?token=bc921e78dc5461022dd5106eb73d3fe0abb95a02862f8ac4
 
-
-
-
 # Enter bash of postgres docker
-
-
-
 
 docker-compose exec pg.itversity.com bash
 
-
-
-
 # Directly enter psql 
-
-
-
 
 docker-compose exec pg.itversity.com psql -U postgres
 
 psql -h localhost -p 5432 -d itversity_sms_db -U itversity_sms_user -W
-
 
 docker container create \
     --name my_pg \
@@ -174,10 +82,7 @@ docker container create \
     -h my_pg \
     -e POSTGRES_PASSWORD=itversity \
     postgres
-
-
+    
 docker exec \
     -it my_pg \
     psql -U postgres
-
-```
